8703f21424a94e75db2ce8a9dcedafb9cd49d016
nippeke <nippeke@gmail.com>
2021-06-14 15:49:58 +0900

    ConstraintLayoutとチェイン

---
+++ ConstraintLayoutSample/app/src/main/java/com/websarva/wings/android/constraintlayoutsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.websarva.wings.android.constraintlayoutsample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
